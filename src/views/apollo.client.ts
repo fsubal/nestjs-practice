9d18b874182357286995556bb294ddcd36bb49de
fsubal <fsubal1102@gmail.com>
2020-01-02 23:43:30 +0900

    dont split files

--- src/views/apollo.client.ts
+++
@@ -1,8 +0,0 @@
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
-
-export function createApolloClient(initial: any) {
-  return new ApolloClient({
-    uri: '/graphql',
-    cache: new InMemoryCache().restore(initial),
-  });
-}
--- src/views/main.client.tsx
+++ src/views/main.client.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { hydrate } from 'react-dom';
 import { App } from './pages/app';
 import { ApolloProvider } from 'react-apollo';
-import { createApolloClient } from './apollo.client';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.querySelector<HTMLDivElement>('#root');
@@ -18,8 +18,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const apolloState = JSON.parse(app.dataset.apollo!);
   console.log(apolloState);
 
+  const client = new ApolloClient({
+    uri: '/graphql',
+    cache: new InMemoryCache().restore(apolloState.ROOT_QUERY),
+  });
+
   hydrate(
-    <ApolloProvider client={createApolloClient(apolloState)}>
+    <ApolloProvider client={client}>
       <App
         initial={JSON.parse(app.dataset.initial!)}
         apolloState={apolloState}
